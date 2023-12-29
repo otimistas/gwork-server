bc4f9092402ff4bacd89a7b92ef39be8cb0e08a5
blank <hayaken3007@i.softbank.jp>
2023-12-29 09:31:20 +0900

    exec initial seeder

--- db/init/postgresql/shema.sql
+++ db/init/postgresql/shema.sql
@@ -78,7 +78,7 @@ CREATE TABLE m_user_permissions (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255) NOT NULL,
     detail TEXT NOT NULL,
-    category_id UUID REFERENCES m_role_categories(id) ON UPDATE RESTRICT ON DELETE RESTRICT NOT NULL
+    category_id UUID REFERENCES m_user_permission_categories(id) ON UPDATE RESTRICT ON DELETE RESTRICT NOT NULL
 );
 CREATE INDEX idx_m_user_permissions_name ON m_user_permissions(name);
 
@@ -143,7 +143,7 @@ CREATE TABLE t_chats (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     content TEXT NOT NULL,
     channel_id UUID REFERENCES m_channels(id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
-	user_id UUID REFERENCES m_users(id) ON UPDATE SET NULL ON DELETE SET NULL,
-	posted_at TIMESTAMPTZ NOT NULL,
+    user_id UUID REFERENCES m_users(id) ON UPDATE SET NULL ON DELETE SET NULL,
+    posted_at TIMESTAMPTZ NOT NULL
 );
 CREATE INDEX idx_m_chats_posted_at ON t_chats(posted_at);
