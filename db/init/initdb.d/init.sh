af6999737b7b1b3c6f20aa0b3a5b3d4d682736f4
blank <hayaken3007@i.softbank.jp>
2023-12-29 13:16:52 +0900

    fit change to migrate sql files

--- db/init/initdb.d/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" -f /scripts/shema.sql
-psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" -f /scripts/group_permission_seeds.sql
-psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" -f /scripts/role_seeds.sql
-psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" -f /scripts/storage_permission_seeds.sql
