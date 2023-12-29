cea85b63ff9b77836413c1229394ad8ae9ed5108
blank <hayaken3007@i.softbank.jp>
2023-12-29 13:09:40 +0900

    write config test

---
+++ internal/config/config_test.go
@@ -0,0 +1,282 @@
+package config_test
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+
+	"github.com/otimistas/gwork-server/internal/config"
+)
+
+func TestGet(t *testing.T) {
+	// Unset environment variables for test
+	envKeys := []string{
+		"PORT",
+		"DB_CONNECTION",
+		"DB_HOST",
+		"DB_PORT",
+		"DB_NAME",
+		"DB_USERNAME",
+		"DB_PASSWORD",
+		"APP_DEBUG",
+		"APP_ENV",
+		"FAKE_TIME",
+	}
+	for _, v := range envKeys {
+		t.Setenv(v, "")
+		os.Unsetenv(v)
+	}
+
+	cases := []struct {
+		name   string
+		env    map[string]string
+		out    *config.Config
+		failed bool
+	}{
+		{
+			name: "minimum",
+			env: map[string]string{
+				"DB_NAME":     "app",
+				"DB_USERNAME": "user",
+			},
+			out: &config.Config{
+				Port:         50051,
+				DBConnection: "pgsql",
+				DBHost:       "localhost",
+				DBPort:       5432,
+				DBName:       "app",
+				DBUsername:   "user",
+				AppEnv:       "production",
+			},
+		},
+		{
+			name: "full",
+			env: map[string]string{
+				"PORT":          "3000",
+				"DB_CONNECTION": "mysql",
+				"DB_HOST":       "db",
+				"DB_PORT":       "9999",
+				"DB_NAME":       "app",
+				"DB_USERNAME":   "user",
+				"DB_PASSWORD":   "password",
+				"APP_DEBUG":     "true",
+				"APP_ENV":       "staging",
+				"FAKE_TIME":     "true",
+			},
+			out: &config.Config{
+				Port:         3000,
+				DBConnection: "mysql",
+				DBHost:       "db",
+				DBPort:       9999,
+				DBName:       "app",
+				DBUsername:   "user",
+				DBPassword:   "password",
+				AppDebug:     true,
+				AppEnv:       "staging",
+				FakeTime: config.FakeTimeMode{
+					Enabled: true,
+					Time:    config.DefaultFakeTime,
+				},
+			},
+		},
+		{
+			name: "FAKE_TIME is RFC3339 string",
+			env: map[string]string{
+				"DB_NAME":     "app",
+				"DB_USERNAME": "user",
+				"FAKE_TIME":   "2023-01-02T12:34:56Z",
+			},
+			out: &config.Config{
+				Port:         50051,
+				DBConnection: "pgsql",
+				DBHost:       "localhost",
+				DBPort:       5432,
+				DBName:       "app",
+				DBUsername:   "user",
+				AppEnv:       "production",
+				FakeTime: config.FakeTimeMode{
+					Enabled: true,
+					Time:    time.Date(2023, 1, 2, 12, 34, 56, 0, time.UTC),
+				},
+			},
+		},
+		{
+			name: "FAKE_TIME is true",
+			env: map[string]string{
+				"DB_NAME":     "app",
+				"DB_USERNAME": "user",
+				"FAKE_TIME":   "true",
+			},
+			out: &config.Config{
+				Port:         50051,
+				DBConnection: "pgsql",
+				DBHost:       "localhost",
+				DBPort:       5432,
+				DBName:       "app",
+				DBUsername:   "user",
+				AppEnv:       "production",
+				FakeTime: config.FakeTimeMode{
+					Enabled: true,
+					Time:    config.DefaultFakeTime,
+				},
+			},
+		},
+		{
+			name: "FAKE_TIME is 1",
+			env: map[string]string{
+				"DB_NAME":     "app",
+				"DB_USERNAME": "user",
+				"FAKE_TIME":   "1",
+			},
+			out: &config.Config{
+				Port:         50051,
+				DBConnection: "pgsql",
+				DBHost:       "localhost",
+				DBPort:       5432,
+				DBName:       "app",
+				DBUsername:   "user",
+				AppEnv:       "production",
+				FakeTime: config.FakeTimeMode{
+					Enabled: true,
+					Time:    config.DefaultFakeTime,
+				},
+			},
+		},
+		{
+			name: "FAKE_TIME is false",
+			env: map[string]string{
+				"DB_NAME":     "app",
+				"DB_USERNAME": "user",
+				"FAKE_TIME":   "false",
+			},
+			out: &config.Config{
+				Port:         50051,
+				DBConnection: "pgsql",
+				DBHost:       "localhost",
+				DBPort:       5432,
+				DBName:       "app",
+				DBUsername:   "user",
+				AppEnv:       "production",
+			},
+		},
+		{
+			name: "FAKE_TIME is 0",
+			env: map[string]string{
+				"DB_NAME":     "app",
+				"DB_USERNAME": "user",
+				"FAKE_TIME":   "0",
+			},
+			out: &config.Config{
+				Port:         50051,
+				DBConnection: "pgsql",
+				DBHost:       "localhost",
+				DBPort:       5432,
+				DBName:       "app",
+				DBUsername:   "user",
+				AppEnv:       "production",
+			},
+		},
+		{
+			name: "FAKE_TIME is empty string",
+			env: map[string]string{
+				"DB_NAME":     "app",
+				"DB_USERNAME": "user",
+				"FAKE_TIME":   "",
+			},
+			out: &config.Config{
+				Port:         50051,
+				DBConnection: "pgsql",
+				DBHost:       "localhost",
+				DBPort:       5432,
+				DBName:       "app",
+				DBUsername:   "user",
+				AppEnv:       "production",
+			},
+		},
+		{
+			name: "invalid PORT",
+			env: map[string]string{
+				"PORT":        "invalid",
+				"DB_NAME":     "app",
+				"DB_USERNAME": "user",
+			},
+			failed: true,
+		},
+		{
+			name: "invalid DB_PORT",
+			env: map[string]string{
+				"DB_PORT":     "invalid",
+				"DB_NAME":     "app",
+				"DB_USERNAME": "user",
+			},
+			failed: true,
+		},
+		{
+			name: "invalid APP_ENV",
+			env: map[string]string{
+				"APP_ENV":     "invalid",
+				"DB_NAME":     "app",
+				"DB_USERNAME": "user",
+			},
+			failed: true,
+		},
+		{
+			name: "invalid APP_DEBUG",
+			env: map[string]string{
+				"APP_DEBUG":   "invalid",
+				"DB_NAME":     "app",
+				"DB_USERNAME": "user",
+			},
+			failed: true,
+		},
+		{
+			name: "invalid FAKE_TIME",
+			env: map[string]string{
+				"DB_NAME":     "app",
+				"DB_USERNAME": "user",
+				"FAKE_TIME":   "invalid",
+			},
+			failed: true,
+		},
+		{
+			name: "missing DB_NAME",
+			env: map[string]string{
+				"DB_USERNAME": "user",
+			},
+			failed: true,
+		},
+		{
+			name: "missing DB_USERNAME",
+			env: map[string]string{
+				"DB_NAME": "app",
+			},
+			failed: true,
+		},
+	}
+
+	for _, v := range cases {
+		t.Run(v.name, func(tt *testing.T) {
+			for key, value := range v.env {
+				tt.Setenv(key, value)
+			}
+
+			cfg, err := config.Get()
+			switch {
+			case err != nil && !v.failed:
+				tt.Fatalf("unexpected error: %+v", err)
+			case err == nil && v.failed:
+				tt.Fatal("unexpected success")
+			case err != nil && v.failed:
+				// pass
+				tt.Logf("expected error: %+v", err)
+				return
+			}
+
+			if diff := cmp.Diff(v.out, cfg); diff != "" {
+				tt.Errorf("unexpected result:\n%s", diff)
+			}
+		})
+	}
+}
