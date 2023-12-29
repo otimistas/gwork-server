fd0b509392aaa7b7460701696a7a99feaf64df3b
blank <hayaken3007@i.softbank.jp>
2023-12-29 12:37:29 +0900

    migrate  raw to migrate on postgresql

---
+++ db/migrations/postgresql/000001_create_m_users_table.up.sql
@@ -0,0 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE m_users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(255) NOT NULL,
+    last_logged_in_at TIMESTAMP,
+    created_by UUID REFERENCES m_users(id) ON UPDATE SET NULL ON DELETE SET NULL,
+    from_system BOOLEAN NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
+);
+CREATE INDEX idx_m_users_name ON m_users(name);
+CREATE INDEX idx_m_users_last_logged_in_at ON m_users(last_logged_in_at);
---
+++ db/migrations/postgresql/000013_create_t_group_storages_permission_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE t_group_storage_permission (
+    storage_id UUID REFERENCES m_storages(id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+    group_id UUID REFERENCES m_groups(id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+    attached_by UUID REFERENCES m_users(id) ON UPDATE SET NULL ON DELETE SET NULL,
+    last_changed_by UUID REFERENCES m_users(id) ON UPDATE SET NULL ON DELETE SET NULL,
+    attached_at TIMESTAMPTZ NOT NULL,
+    last_changed_at TIMESTAMPTZ NOT NULL,
+    PRIMARY KEY (storage_id, group_id)
+);
